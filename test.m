36701374f0869e646d27b745b24e1e541ba45fba
ImageVisioner <ImageVisioner@outlook.com>
2023-10-30 08:55:26 +0800

    something change
    something change

--- test.m
+++ test.m
@@ -5,5 +5,6 @@ figure,surf(X,Y,Z)
 
 % texzlabel('z_{label}',15)
 % 设置带有复杂LaTeX公式的Z轴标签
-texzlabel('\frac{a_{1}}{a_{2}} = \sqrt{b_{1}^{n}}', 25)
+h=texzlabel('\frac{a_{1}}{a_{2}} = \sqrt{b_{1}^{n}}', 25);
+fig2eps(h)
 
